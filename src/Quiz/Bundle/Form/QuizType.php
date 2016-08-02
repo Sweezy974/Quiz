bcaabc4525b99645b1927c0c50785b50382756b4
AshFromReunion <ash-concept@hotmail.fr>
2016-08-02 14:17:21 +0400

    crud

---
+++ src/Quiz/Bundle/Form/QuizType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Quiz\Bundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class QuizType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('question')
+            ->add('reponse1')
+            ->add('reponse2')
+            ->add('reponse3')
+            ->add('reponse4')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Quiz\Bundle\Entity\Quiz'
+        ));
+    }
+}
